368bf446434eba87e2490987ba3f7091c805cdab
Harvir R <136796141+HarvirRajasanci@users.noreply.github.com>
2024-07-25 01:24:28 -0700

    Updated postRouters.ts

--- routers/postRouters.ts
+++ routers/postRouters.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
 import { ensureAuthenticated } from "../middleware/checkAuth";
-import { getPost, getPosts } from "../fake-db";
+import { getPost, getPosts, addPost, editPost, deletePost, addComment } from "../fake-db";
+import { TUser } from "../types";
 
 router.get("/", async (req: Request, res: Response) => {
   const posts = await getPosts(20);
@@ -9,44 +10,75 @@ router.get("/", async (req: Request, res: Response) => {
   res.render("posts", { posts, user });
 });
 
-router.get("/create", ensureAuthenticated, (req, res) => {
+router.get("/create", ensureAuthenticated, (req: Request, res: Response) => {
   res.render("createPosts");
 });
 
-router.post("/create", ensureAuthenticated, async (req, res) => {
+router.post("/create", ensureAuthenticated, async (req: Request, res: Response) => {
   // ⭐ TODO
-  const content = req.body;
-  console.log(content);
+  const content = await req.body;
+
+  if(content.title === "" || content.description === "" || content.subgroup === ""){
+    return res.status(400).send("<h1>All fields (except link) are required.<h1>");
+  }
+  
+  const user = await req.user as TUser;
+  const post = addPost(content.title, content.link, user.id, content.description, content.subgroup);
+  res.redirect("/posts/show/" + post.id);
 });
 
 router.get("/show/:postid", async (req: Request, res: Response) => {
   // ⭐ TODO
-  const postId = req.params.postid;
+  const postId = await req.params.postid;
   const post = await getPost(Number(postId));
-  res.render("individualPost", { post });
+  const comments = await post.comments;
+  const user = await req.user as TUser;
+  res.render("individualPost", { post, comments, user });
 });
 
-router.get("/edit/:postid", ensureAuthenticated, async (req, res) => {
+router.get("/edit/:postid", ensureAuthenticated, async (req: Request, res: Response) => {
+  const postId = await req.params.postid;
+  res.render("editPost", { postId });
+});
+
+router.post("/edit/:postid", ensureAuthenticated, async (req: Request, res: Response) => {
+  const postId = await req.params.postid;
+  const content = await req.body;
+  const post = getPost(Number(postId));
+
+  await editPost(Number(postId), {title: content.title, link: content.link, description: content.description, subgroup: content.subgroup});
+
+  res.redirect("/posts/show/" + postId);
+});
+
+router.get("/deleteconfirm/:postid", ensureAuthenticated, async (req: Request, res: Response) => {
   // ⭐ TODO
+  const postId = await req.params.postid;
+  res.render("deletePost", { postId });
 });
 
-router.post("/edit/:postid", ensureAuthenticated, async (req, res) => {
+router.post("/delete/:postid", ensureAuthenticated, async (req: Request, res: Response) => {
   // ⭐ TODO
+  const postId = await req.params.postid;
+  
+  await deletePost(Number(postId));
+
+  res.redirect("/posts");
 });
 
-router.get("/deleteconfirm/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
-});
-
-router.post("/delete/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
-});
-
-router.post(
-  "/comment-create/:postid",
-  ensureAuthenticated,
-  async (req, res) => {
+router.post("/comment-create/:postid",ensureAuthenticated, async (req: Request, res: Response) => {
     // ⭐ TODO
+    const postId = await req.params.postid;
+    const content = await req.body;
+    const user = await req.user as TUser;
+
+    if(content.description === ""){
+      return res.status(400).send("<h1>Description is required to post a comment.</h1>");
+    }
+
+    addComment(Number(postId), user.id, content.description);
+
+    res.redirect("/posts/show/" + postId)
   }
 );
 
