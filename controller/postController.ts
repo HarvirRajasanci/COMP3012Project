d00c3d6c21fc86a5d453f396db6ac7e2fd9967bc
Harvir R <136796141+HarvirRajasanci@users.noreply.github.com>
2024-07-26 00:00:20 -0700

    Updated postController.ts

--- controller/postController.ts
+++ controller/postController.ts
@@ -1,10 +1,26 @@
 import * as db from "../fake-db";
+import { TVote } from "../types";
 
 export const getPostsBySubgroup = async (subgroup: string) => {
-    let posts = db.getPosts(5, subgroup);
+    const ALL_POSTS = -1;
+    let posts = db.getPosts(ALL_POSTS, subgroup);
+    posts.sort((a, b) => b.timestamp - a.timestamp);
     if (posts) {
       return posts;
     }
     return null;
   };
-  
+
+  export const addVote = async(vote: TVote) => {
+
+    const newVote = await db.addVoteToDb(vote.user_id, vote.post_id, vote.value);
+
+    if (newVote) {
+      return newVote;
+    }
+    return null;
+  }
+
+  export const removeVote = async(vote: TVote) => {
+
+  }
