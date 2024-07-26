ae8a4c64b60023d6878b0dd7384baa32976d8a2e
Harvir R <136796141+HarvirRajasanci@users.noreply.github.com>
2024-07-25 18:47:47 -0700

    Updated subsRouters.ts

--- routers/subsRouters.ts
+++ routers/subsRouters.ts
@@ -1,16 +1,20 @@
 const { ensureAuthenticated } = require("../middleware/checkAuth");
 import express, { Request, Response } from "express";
 import * as database from "../controller/postController";
+import { getSubs } from "../fake-db";
 const router = express.Router();
 
 router.get("/list", async (req: Request, res: Response) => {
   // ⭐ TODO
-  res.render("subs");
+  const subgroups = await getSubs();
+  res.render("subs", { subgroups });
 });
 
 router.get("/show/:subname", async (req: Request, res: Response) => {
   // ⭐ TODO
-  res.render("sub");
+  const subName = await req.params.subname;
+  const posts = await database.getPostsBySubgroup(subName);
+  res.render("sub", { posts });
 });
 
 export default router;
